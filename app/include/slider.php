68c9f10def78fe652b797fd8fe07add2b76bd5d9
iwebnal <iwebnal@gmail.com>
2017-03-21 11:32:58 +0300

    slider

--- app/include/slider.php
+++ app/include/slider.php
@@ -1,1 +1,26 @@
-slider.php
+<section class="ah-slider">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-sliderbox">
+					<div class="item" style="background: url(../img/slide1.jpg) center center no-repeat; background-size: cover;">
+						<div class="ah-slidecontent">
+							<span class="ah-slidecontent-span1">Скидки до</span>
+							<span class="ah-slidecontent-span2">50%</span>
+							<span class="ah-slidecontent-span3">на товары в рубрике</span>
+							<span class="ah-slidecontent-span4">РАСПРОДАЖА!!!</span>
+						</div>
+					</div>
+					<div class="item" style="background: url(../img/slide2.jpg) center center no-repeat; background-size: cover;">
+						<div class="ah-slidecontent">
+							<span class="ah-slidecontent-span1">Скидки до</span>
+							<span class="ah-slidecontent-span2">50%</span>
+							<span class="ah-slidecontent-span3">на товары в рубрике</span>
+							<span class="ah-slidecontent-span4">РАСПРОДАЖА!!!</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,28 @@
 $(function() {
 
+	$('.ah-sliderbox').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    autoplay:false,
+	    smartSpeed:1000,
+	    autoplayTimeout:2000,
+	    dots: true,
+	    navText:['<span class="prev_arrs1"></span>', '<span class="next_arrs1"></span>'],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+
+
+
+
 });
