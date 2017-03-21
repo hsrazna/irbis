fa168fee4141725f9c4cdd6e9b262fa4adb83808
hsrazna <anzar.terek@gmail.com>
2017-03-21 17:52:34 +0300

    header

--- app/include/header.php
+++ app/include/header.php
@@ -1,1 +1,144 @@
-header.php
+<header class="az-header hidden-xs">
+	<div class="az-menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="az-table">
+							<li><a href="#">Каталог</a></li>
+							<li><a href="#">Доставка и оплата</a></li>
+							<li><a href="#">Скидки и акции</a></li>
+							<li><a href="#">Магазины</a></li>
+							<li><a href="#">Пункты выдачи заказов</a></li>
+							<li><a href="#">О компании</a></li>
+							<li><a href="#">Оптовикам</a></li>
+							<li><a href="#">Контакты</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="az-header-middle">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="az-header-middle-wrap clearfix">
+						<div class="pull-left az-logo">
+							<img src="img/logo.png" alt="логотип">
+						</div>
+						<div class="pull-left az-header-icons">
+							<div class="az-icons clearfix">
+								<div class="az-header-icon">
+									<span class="az-icon-header">Выгодно</span>
+									<span class="az-icon-cont">Бесплатная доставка по РФ от 10000 р.</span>
+								</div>
+								<div class="az-header-icon style2">
+									<span class="az-icon-header">Удобно</span>
+									<span class="az-icon-cont">620 пунктов выдачи заказов</span>
+								</div>
+								<div class="az-header-icon style3">
+									<span class="az-icon-header">Безопасно</span>
+									<span class="az-icon-cont">Оплата Наличными при получении</span>
+								</div>
+							</div>
+						</div>
+						<div class="pull-right az-header-phones clearfix">
+							<div class="az-phones-inner">
+								<div class="az-phones-inner-top">
+									<a href="#">8 (499) <strong>703 44 27</strong></a>
+									<a href="#">8 (800) <strong>250 86 88</strong></a>
+								</div>
+								<div class="az-phones-inner-bottom">
+									<span>Звонок бесплатный</span>
+								</div>
+							</div>
+							<div class="az-call-back-inner">
+								<a href="#">Заказать звонок</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="az-header-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="az-hb-wrap clearfix">
+						<div class="az-hb-left">
+							<span>Интернет-магазин<br>снаряжения и экипировки</span>
+						</div>
+						<div class="az-hb-middle">
+							<form action="/">
+								<input type="text" placeholder="Я ищу... например, велорюкзак">
+								<input type="submit" value="Поиск">
+							</form>
+						</div>
+						<div class="az-hb-right">
+							<div class="az-cart">
+								<a href="#" class="az-cart-top">Корзина:</a>
+								<span class="az-cart-summ">0 руб.</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<header class="az-header2 hidden-sm hidden-md hidden-lg">
+	<div class="az-header-top2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="pull-left az-logo">
+						<img src="img/logo-mobile.png" alt="логотип">
+					</div>
+					<div class="pull-right az-header-phones clearfix">
+						<div class="az-phones-inner">
+							<div class="az-phones-inner-top">
+								<a href="#">8 (499) <strong>703 44 27</strong></a>
+								<a href="#">8 (800) <strong>250 86 88</strong></a>
+							</div>
+							<div class="az-phones-inner-bottom">
+								<span>Звонок бесплатный</span>
+							</div>
+						</div>
+						<div class="az-call-back-inner">
+							<a href="#">Заказать звонок</a>
+						</div>
+						<div class="az-ht2-left">
+							<div class="az-cart">
+								<a href="#" class="az-cart-top">Корзина:</a>
+								<span class="az-cart-summ">0 руб.</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="az-header-middle2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="az-hm2-left">
+						<div class="az-hb-middle">
+							<form action="/">
+								<input type="text" placeholder="Я ищу... например, велорюкзак">
+								<input type="submit" value="Поиск">
+							</form>
+						</div>
+						<div class="az-hb-left">
+							<span>Интернет-магазин<br>снаряжения и экипировки</span>
+						</div>
+					</div>
+					<div class="az-hm2-right">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
