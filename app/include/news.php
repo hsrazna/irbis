8298bb6cfd27996356288458a6244fc09021d852
hsrazna <anzar.terek@gmail.com>
2017-03-22 12:54:27 +0300

    news

--- app/include/news.php
+++ app/include/news.php
@@ -1,1 +1,55 @@
-news.php
+<section class="az-news">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2><span>Новости</span></h2>
+				<div class="az-row clearfix">
+					<div class="az-col">
+						<div class="az-news-item">
+							<div class="az-cat">
+								<a href="#">
+									<img src="img/az-news1.jpg" alt="Промышленный альпинизм">
+									<span>14.02.2017</span>
+								</a>
+							</div>
+							<a href="#">Всегда в наличии туристические коврики</a>
+						</div>
+					</div>
+					<div class="az-col">
+						<div class="az-news-item">
+							<div class="az-cat">
+								<a href="#">
+									<img src="img/az-news2.jpg" alt="Туризм">
+									<span>20.01.2017</span>
+								</a>
+							</div>
+							<a href="#">Плюшки-ватрушки-тюбинги</a>
+						</div>
+					</div>
+					<div class="az-col">
+						<div class="az-news-item">
+							<div class="az-cat">
+								<a href="#">
+									<img src="img/az-news3.jpg" alt="Охота и рыбалка">
+									<span>04.01.2017</span>
+								</a>
+							</div>
+							<a href="#">Поступление снегоступов Inook</a>
+						</div>
+					</div>
+					<div class="az-col hidden-sm hidden-md hidden-lg">
+						<div class="az-news-item">
+							<div class="az-cat">
+								<a href="#">
+									<img src="img/az-news1.jpg" alt="Кемпинг">
+									<span>01.01.2017</span>
+								</a>
+							</div>
+							<a href="#">Плюшки-ватрушки-тюбинги</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
