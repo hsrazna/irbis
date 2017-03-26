05a19b79ed5f84dae4d4840740ad4ab44ad34b7e
iwebnal <iwebnal@gmail.com>
2017-03-26 22:34:54 +0300

    card product

---
+++ app/include/breadcrumbs.php
@@ -0,0 +1,14 @@
+<section class="ah-readcrumbs">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<ul class="ah-readcrumbs-list">
+					<li><a href="#">Каталог</a><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+					<li><a href="#">Альпинизм</a><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+					<li><a href="#">Страховочные системы</a><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+					<li><span>Поясная беседка Ринг Descent</span></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ app/include/card-product.php
@@ -0,0 +1,159 @@
+<section class="ah-cardprod">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-7 col-lg-push-5 col-md-7 col-md-push-5 col-sm-7 col-sm-push-5 col-xs-12">
+	    		<span class="ah-cardprod-title">Поясная беседка Ринг Descent</span>
+	    	</div>
+	    	<div class="clearfix"></div>
+	    	<div class="row">
+		    	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+						<div class="ah-cardprod-imgbox">
+							<img src="img/ah-cardprod1.jpg" class="img-responsive" alt="product">
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<div class="ah-productdetail-box">
+							<span class="ah-productdetail-title">Характеристики товара:</span>
+							<ul class="ah-productdetail-list">
+								<li class="ah-colorcell">
+									<span class="ah-prodlist-cellleft">Вес, кг</span>
+									<span class="ah-prodlist-cellright">440</span>
+								</li>
+								<li>
+									<span class="ah-prodlist-cellleft">Производитель</span>
+									<span class="ah-prodlist-cellright">Ринг</span>
+								</li>
+								<li class="ah-colorcell">
+									<span class="ah-prodlist-cellleft">Тип пряжки</span>
+									<span class="ah-prodlist-cellright">обычная</span>
+								</li>
+								<li>
+									<span class="ah-prodlist-cellleft">Тип системы</span>
+									<span class="ah-prodlist-cellright">поясная<br>беседка</span>
+								</li>
+								<li class="ah-colorcell">
+									<span class="ah-prodlist-cellleft">Назначение</span>
+									<span class="ah-prodlist-cellright">спелео</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<span class="ah-cardprod-textspan">Описание товара:</span>
+						<p class="ah-cardprod-text">Удобная страховочная система для спелеологии и спасательных работ. — Низкое расположение брюшного зажима. — Ножные петли обшиты PVC-тканью, что придает дополнительную жесткость и защищает от трения. — Точки крепления усилены трубчатой стропой. — Три петли для дополнительного снаряжения. Блокировка только основной веревкой или специальным полукруглым карабином. Вес: 440 гр.</p>
+						<div class="ah-cardprod-linkbox">
+							<a href="#" class="ah-cardlinkbox-link1">Товары для сровнение</a>
+							<a href="#" class="ah-cardlinkbox-link1 ah-cardlink1">Отложеные товары</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<div class="ah-productwrapp">
+						<div class="ah-productprice">
+							<span class="ah-productprice-span">780 <span>руб.</span></span>
+						</div>
+						<div class="ah-productdetail1">
+							<span class="ah-productdetail1-span">Наличие на складе: <span>Да</span></span>
+							<ul class="ah-productdetail1-list">
+								<li>
+									<span class="ah-productdetail1-left">Стоимость доставки</span>
+									<span class="ah-productdetail1-right">Срок</span>
+								</li>
+								<li>
+									<span class="ah-productdetail1-left">no: <span>450 руб.</span></span>
+									<span class="ah-productdetail1-right"><span>3-10 дней</span></span>
+								</li>
+							</ul>
+						</div>
+						<div class="ah-productbox">
+							<div class="ah-productbox-table">
+								<div class="ah-productbox-left">
+									<span class="ah-productbox-leftspan">Кол-во:</span>
+									<form class="ah-product-form">
+										<a href="#" class="ah-product-form-min">-</a>
+										<input type="text" placeholder="1">
+										<a href="#" class="ah-product-form-plus">+</a>
+									</form>
+								</div>
+								<div class="ah-productbox-right">
+									<a href="#" class="ah-productbox-rightlink">Купить</a>
+								</div>
+							</div>
+						</div>
+						<div class="ah-productbox1">
+							<form class="ah-product-form1">
+								<label><input type="checkbox" class="ah-check">Сравнить</label>
+								<a href="#">В избранное</a>
+							</form>
+						</div>
+					</div>
+					<div class="ah-productwrapp1">
+						<span class="ah-productwrapp1-span">Оплата наличными при получении, или:</span>
+						<span class="ah-productwrapp1-span1">КУПИ ДЕШЕВЛЕ на 3%,</span>
+						<span class="ah-productwrapp1-span">оплатив заказ картой</span>
+						<span class="ah-productwrapp1-span">VISA / MasterCard</span>
+						<div class="ah-productwrapp11">
+							<span class="ah-productwrapp11-span">*Абсолютная безопасность платежа</span>
+							<span class="ah-productwrapp11-span">*Возврат в течение 1 часа при отмене заказа</span>
+						</div>
+					</div>
+					<div class="ah-productwrapp2">
+						<span class="ah-productwrapp2-title">Почему <span>выгодно</span> покупать в Irbis-shop.ru</span>
+						<div class="ah-productwrapp2-table">
+							<div class="ah-productwrapp2-left">
+								<div class="ah-producticon">
+									<img src="img/ico_save.png" alt="save">
+								</div>
+							</div>
+							<div class="ah-productwrapp2-right">
+								<span class="ah-productwrapp2-rightspan"><span>Безопасная покупка</span> – возможна оплата при получении</span>
+							</div>
+						</div>
+						<div class="ah-productwrapp2-table">
+							<div class="ah-productwrapp2-left">
+								<div class="ah-producticon">
+									<img src="img/ico_fitting.png" alt="fitting">
+								</div>
+							</div>
+							<div class="ah-productwrapp2-right">
+								<span class="ah-productwrapp2-rightspan"><span>Покупаете то, что хотели</span> – возможность просмотра заказа или примерки до оплаты</span>
+							</div>
+						</div>
+						<div class="ah-productwrapp2-table">
+							<div class="ah-productwrapp2-left">
+								<div class="ah-producticon">
+									<img src="img/ico_economy.png" alt="economy">
+								</div>
+							</div>
+							<div class="ah-productwrapp2-right">
+								<span class="ah-productwrapp2-rightspan"><span>Вы экономите</span> – бесплатная доставка от 10000р.</span>
+							</div>
+						</div>
+						<div class="ah-productwrapp2-table">
+							<div class="ah-productwrapp2-left">
+								<div class="ah-producticon">
+									<img src="img/ico_change.png" alt="change">
+								</div>
+							</div>
+							<div class="ah-productwrapp2-right">
+								<span class="ah-productwrapp2-rightspan"><span>Возврат и обмен без проблем</span> – в течение 14 дней с момента покупки</span>
+							</div>
+						</div>
+						<div class="ah-productwrapp2-table">
+							<div class="ah-productwrapp2-left">
+								<div class="ah-producticon">
+									<img src="img/ico_points.png" alt="points">
+								</div>
+							</div>
+							<div class="ah-productwrapp2-right">
+								<span class="ah-productwrapp2-rightspan"><span>Удобно забирать</span> – 620 пунктов выдачи по всей России</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
